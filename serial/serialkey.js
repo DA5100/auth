bcdc8a1446ce3038b8d33752b1456810587d07c1
DA5100 <dramaarena5100@gontor.ac.id>
2025-07-28 14:29:38 +0700

    Input Serial Key

--- serial/serialkey.js
+++ serial/serialkey.js
@@ -1,6 +1,23 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js'
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnRKitQGBX0u8k4COtDTILYxCJuMf7xzE",
+  authDomain: "authdramaarena.firebaseapp.com",     
+  databaseURL: "https://authdramaarena.firebaseio.com",
+  projectId: "authdramaarena",                      
+  storageBucket: "authdramaarena.appspot.com",      
+  messagingSenderId: "348583435302",             
+  appId: "1:348583435302:web:someUniqueWebId",    
+  measurementId: "G-DGF0CP099H"                  
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 const user = firebase.auth().currentUser;
 const inputKey = prompt("Masukkan Serial Key:");
-
 const db = firebase.firestore();
 const keyRef = db.collection("lisensi").doc(lisensi);
 
@@ -14,13 +31,13 @@ keyRef.get().then((doc) => {
   const keyData = doc.data();
 
   if (keyData.used) {
-    alert("Serial key ini sudah digunakan.");
+    alert("Maaf, Serial key ini sudah digunakan.");
     auth.signOut();
     return;
   }
 
   if (keyData.blocked) {
-    alert("Serial key ini diblokir.");
+    alert("Maaf, Serial key ini diblokir.");
     auth.signOut();
     return;
   }
